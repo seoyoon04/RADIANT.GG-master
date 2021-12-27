ea925f133e30e7edef67968d15e1770307a900b4
seoyoon04 <sykim8483@naver.com>
2021-12-27 12:59:14 +0900

    commit

---
+++ fileContainer/index.js
@@ -0,0 +1,18 @@
+// async function requestVal() {
+//     // const data = await fetch("https://valorant-api.com/v1/contenttiers");
+//     // console.log(data);
+//     fetch('https://valorant-api.com/v1/agents')
+//         .then(res => res.json())
+//         .then(res=>{
+//             console.log('success');
+//     });
+// }
+
+// // requestVal();
+
+// const url = "https://valorant-api.com/v1/agents";
+
+
+// fetch(url).then(res=>res.json()).then(res=>{
+//     console.log('fetch success');
+// });
